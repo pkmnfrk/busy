b8ddf6435b17c3977bb3f4acd1711d906b50197e
Mike Caron <mcaron@klick.com>
2014-03-27 09:08:53 -0400

    Yay, it actually works now!

--- MyButtonThing/Form1.cs
+++ MyButtonThing/Form1.cs
@@ -11,6 +11,9 @@ using System.Windows.Forms;
 using UsbHid;
 using UsbHid.USB.Classes.Messaging;
 
+using Genome;
+using Microsoft.Win32;
+
 namespace MyButtonThing
 {
     public partial class Form1 : Form
@@ -19,23 +22,82 @@ namespace MyButtonThing
         ActionState currentState = ActionState.None;
         bool closing = false;
 
+        DateTime? startDate;
+
+        UserContext userContext;
+
 
         public Form1()
         {
             InitializeComponent();
 
             button = new CanFocusButton();
-            button.State = ButtonState.TalkToMe;
+            button.State = ButtonState.Red;
             button.AutoManageButton = false;
 
             button.ConnectedChanged += button_ConnectedChanged;
             button.ButtonPress += button_ButtonPress;
+            
+            notifyIcon1.Icon = button.IsConnected ? icons.icoRed : icons.icoGrey;
+            
+            using (var regKey = Registry.CurrentUser.CreateSubKey(registryKey, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
 
-            btnTryAgain.Visible = !button.IsConnected;
+                var tmp = regKey.GetValue("StartDate", DateTime.MinValue.ToBinary());
 
-            notifyIcon1.Icon = icons.icoGrey;
+                if (tmp != null)
+                {
+                    var start = DateTime.FromBinary((long)tmp);
+                    var action = (string)regKey.GetValue("ActionState", "");
+
+                    if (start.Date == DateTime.Now.Date)
+                    {
+                        var result = MessageBox.Show("Would you like to continue the " + action + " that you started at " + start + "?", "Resume task", MessageBoxButtons.YesNo);
+
+                        if (result == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            startDate = start;
+                            currentState = (ActionState)Enum.Parse(typeof(ActionState), action);
+                            UpdateIcon();
+                        }
+
+                    }
+                }
+
+                var ctx = (string)regKey.GetValue("UserContext", null);
+                if (ctx != null)
+                {
+                    userContext = new UserContext(ctx);
+
+                    //validate that the context is still good
+                    var currentUser = User.GetCurrentUser(userContext);
+                    if (currentUser == null) //nope
+                        userContext = null;
+                }
+
+                if(userContext == null)
+                {
+                    using (var dlg = new Genome.LoginForm())
+                    {
+                        var result = dlg.ShowDialog();
+
+                        if (result == System.Windows.Forms.DialogResult.OK)
+                        {
+                            userContext = dlg.UserContext;
+                            regKey.SetValue("UserContext", userContext.ToString());
+                        }
+
+                    }
+                }
+
+                
+            }
+
+            button.State = ButtonState.Green;
+            notifyIcon1.Icon = button.IsConnected ? icons.icoGreen : icons.icoGrey;
         }
 
+        private bool alreadyShowing = false;
 
         void button_ButtonPress(object sender, EventArgs e)
         {
@@ -43,34 +105,96 @@ namespace MyButtonThing
 
             Action action = () =>
             {
-                using (var picker = new ActionChangeDialog(currentState))
+                if (alreadyShowing) return;
+                alreadyShowing = true;
+                try
                 {
-
-                    var result = picker.ShowDialog(this);
-                    if (result == System.Windows.Forms.DialogResult.OK)
+                    using (var picker = new ActionChangeDialog(currentState))
                     {
-                        currentState = picker.NewState;
+
+                        button.State = ButtonState.Red;
+                        notifyIcon1.Icon = icons.icoRed;
+
+                        var result = picker.ShowDialog(this);
+                        if (closing) return;
+
+                        if (result == System.Windows.Forms.DialogResult.OK)
+                        {
+                            StopTracking();
+                            currentState = picker.NewState;
+                            StartTracking();
+
+                        }
                     }
+
+                    notifyIcon1.Icon = icons.icoGreen;
+                    button.State = ButtonState.Green;
+
+                    UpdateIcon();
+                }
+                finally
+                {
+                    alreadyShowing = false;
                 }
 
-                notifyIcon1.Icon = icons.icoGreen;
-                button.State = ButtonState.Green;
-
-                UpdateIcon();
 
             };
 
-            button.State = ButtonState.Red;
-            notifyIcon1.Icon = icons.icoRed;
+            
 
             if (this.InvokeRequired)
             {
-                this.Invoke(action);
+                this.BeginInvoke(action);
             }
             else
             {
                 action();
             }
+        }
+
+        private string registryRoot = "HKEY_CURRENT_USER";
+        private string registryKey = @"Software\Klick\TaskSwitcher";
+
+        private void StartTracking()
+        {
+            startDate = DateTime.Now;
+
+            if (currentState != ActionState.None)
+            {
+
+                Registry.SetValue(registryRoot + "\\" + registryKey, "StartDate", startDate.Value.ToBinary(), RegistryValueKind.QWord);
+                Registry.SetValue(registryRoot + "\\" + registryKey, "ActionState", currentState.ToString());
+            }
+        }
+
+        private void StopTracking()
+        {
+            //Registry.CurrentUser.DeleteSubKey(registryKey, false);
+
+            using (var key = Registry.CurrentUser.OpenSubKey(registryKey, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.DeleteValue("StartDate", false);
+                key.DeleteValue("ActionState", false);
+            }
+
+            if (currentState == ActionState.None) return;
+
+            var timeDiff = DateTime.Now - startDate.Value;
+
+            timeDiff = new TimeSpan(timeDiff.Ticks * 60); //fudge-factor for testing (1 second = 1 minute)
+
+            var request = new TimeEntryPostRequest();
+
+            request.Date = startDate.Value;
+            request.Duration = (int)timeDiff.TotalMinutes;
+            request.UserID = 4517;
+            request.Type = "Schedule-Note";
+            request.Note = currentState.ToString();
+            request.userFriendlyDuration = (DateTime.Now - startDate).ToString();
+
+            request.Send(userContext);
+
+            currentState = ActionState.None;
         }
 
         void button_ConnectedChanged(object sender, ConnectedChangedEventArgs e)
@@ -79,13 +203,12 @@ namespace MyButtonThing
 
             Action action = () =>
                 {
-                    btnTryAgain.Visible = !e.IsConnected;
                     notifyIcon1.Icon = e.IsConnected ? icons.icoGreen : icons.icoGrey;
                 };
 
             if (this.InvokeRequired)
             {
-                this.Invoke(action);
+                this.BeginInvoke(action);
             }
             else
             {
